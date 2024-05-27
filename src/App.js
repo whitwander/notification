15b5bd1aa7c497b8fdb31aea8cb97bc2bef6b91b
WhitWander <vcmikhaelcp@gmail.com>
2024-05-27 18:45:01 -0300

    set

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+import Header from "./components/Header/Header";
+import "./App.css"
 
 function App() {
-  return null;
+  return (
+    <Header />
+  )
 }
 
 export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import "./header.css" 
+
+export default function Header() {
+    return (
+        <div className="header">
+            <div>
+                <h1>Notifications</h1>
+                <p>3</p>
+            </div>
+            <p>Mark all as read</p>
+        </div>
+    )
+}

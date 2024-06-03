872e45002333ce0224ac4b79d82f979fbed018a5
WhitWander <vcmikhaelcp@gmail.com>
2024-06-03 19:54:58 -0300

    adding boxes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Header from "./components/Header/Header";
 import "./App.css"
+import Box from "./components/Box/Box";
 
 function App() {
   return (
+    <>
     <Header />
+    <Box />
+    </>
   )
 }
 

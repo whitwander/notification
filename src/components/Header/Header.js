a972d3a4937efdf37cb3c7019e7b1a8d8cdb28ff
WhitWander <vcmikhaelcp@gmail.com>
2024-06-03 19:36:45 -0300

    notification header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import "./header.css"
 export default function Header() {
     return (
         <div className="header">
-            <div>
+            <div className="header-notification">
                 <h1>Notifications</h1>
                 <p>3</p>
             </div>
